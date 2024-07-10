a0163cbc96de9b75818b60baa647131e727c933e
juan <jmrivasconcepcion@gmail.com>
2024-07-10 09:57:36 +0200

    update

---
+++ workspaces/deflate/deflate_eval.py
@@ -0,0 +1,116 @@
+from model import System
+from gradient import *
+from analysis import HolisticFPAnalysis, HolisticGlobalEDFAnalysis
+from assignment import PDAssignment, HOPAssignment, repr_priorities,EQFAssignment, EQSAssignment
+from vector import VectorHolisticFPBatchCosts, PrioritiesMatrix
+from evaluation import SchedRatioEval
+from examples import get_system
+from random import Random
+from fast_analysis import FastHolisticFPAnalysis
+from functools import partial
+
+
+def gdpa_pd_fp_vector(system: System) -> bool:
+    analysis = HolisticFPAnalysis(limit_factor=10, reset=False)
+    extractor = PriorityExtractor()
+    cost_function = InvslackCost(extractor=extractor, analysis=analysis)
+    stop_function = StandardStop(limit=100)
+    delta_function = AvgSeparationDelta(factor=1.5)
+    batch_cost_function = VectorHolisticFPBatchCosts(PrioritiesMatrix())
+    gradient_function = StandardGradient(delta_function=delta_function,
+                                         batch_cost_function=batch_cost_function)
+    update_function = NoisyAdam()
+    optimizer = StandardGradientDescent(extractor=extractor,
+                                        cost_function=cost_function,
+                                        stop_function=stop_function,
+                                        gradient_function=gradient_function,
+                                        update_function=update_function,
+                                        verbose=False)
+
+    pd = PDAssignment(normalize=True)
+    pd.apply(system)
+    optimizer.apply(system)
+    analysis.apply(system)
+    return system.is_schedulable()
+
+
+def gdpa_pd_fp_vector_deflate(system: System) -> bool:
+    analysis = HolisticFPAnalysis(limit_factor=10, reset=False)
+    extractor = PriorityExtractor()
+    cost_function = InvslackCost(extractor=extractor, analysis=analysis)
+    stop_function = FixedIterationsStop(iterations=100)
+    delta_function = AvgSeparationDelta(factor=1.5)
+    batch_cost_function = VectorHolisticFPBatchCosts(PrioritiesMatrix())
+    gradient_function = StandardGradient(delta_function=delta_function,
+                                         batch_cost_function=batch_cost_function)
+    update_function = NoisyAdam()
+    optimizer = StandardGradientDescent(extractor=extractor,
+                                        cost_function=cost_function,
+                                        stop_function=stop_function,
+                                        gradient_function=gradient_function,
+                                        update_function=update_function,
+                                        verbose=False,
+                                        deflate=0.8)
+
+    pd = PDAssignment(normalize=True)
+    pd.apply(system)
+    optimizer.apply(system)
+    analysis.apply(system)
+    return system.is_schedulable()
+
+
+def pd_fp(system: System) -> bool:
+    analysis = HolisticFPAnalysis(limit_factor=10, reset=False)
+    pd = PDAssignment(normalize=True)
+    pd.apply(system)
+    analysis.apply(system)
+    return system.is_schedulable()
+
+
+def eqs_fp(system: System) -> bool:
+    analysis = HolisticFPAnalysis(limit_factor=10, reset=False)
+    eqs = EQSAssignment()
+    eqs.apply(system)
+    analysis.apply(system)
+    return system.is_schedulable()
+
+
+def eqf_fp(system: System) -> bool:
+    analysis = HolisticFPAnalysis(limit_factor=10, reset=False)
+    eqf = EQFAssignment()
+    eqf.apply(system)
+    analysis.apply(system)
+    return system.is_schedulable()
+
+
+def hopa_fp(system: System) -> bool:
+    analysis = HolisticFPAnalysis(limit_factor=10, reset=False)
+    hopa = HOPAssignment(analysis=analysis)
+    hopa.apply(system)
+    analysis.apply(system)
+    return system.is_schedulable()
+
+
+if __name__ == '__main__':
+    # create population of examples
+    rnd = Random(42)
+    size = (3, 4, 3)  # flows, tasks, procs
+    n = 50
+    systems = [get_system(size, rnd, balanced=True, name=str(i),
+                          deadline_factor_min=0.5,
+                          deadline_factor_max=1) for i in range(n)]
+
+    # utilizations between 50 % and 90 %
+    utilizations = np.linspace(0.5, 0.9, 20)
+
+    tools = [("gdpa", gdpa_pd_fp_vector),
+             ("gdpa_deflate", gdpa_pd_fp_vector_deflate),
+             ("hopa", hopa_fp),
+             ("pd", pd_fp),
+             ("eqs", eqs_fp),
+             ("eqf", eqf_fp)]
+
+    labels, funcs = zip(*tools)
+    runner = SchedRatioEval("test", labels=labels, funcs=funcs,
+                            systems=systems, utilizations=utilizations, threads=6)
+    runner.run()
